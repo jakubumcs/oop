5eb6cabf3f4a5a7eb98a3cf6906cacf2845d467a
jakub <kubaa42@o2.pl>
2026-05-10 22:13:03 +0200

    final

--- src/SvgScene.java
+++ src/SvgScene.java
@@ -1,62 +1,45 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
 
 public class SvgScene {
-
-    // POLIMORFIZM W PRAKTYCE:
-    // Tablica przechowuje referencje typu Shape, ale mogą wskazywać na Polygon LUB Ellipse.
-    // Java "wie" w runtime jaki jest prawdziwy typ obiektu i wywoła właściwą metodę toSvg().
-    // Zasada: programuj do interfejsu/klasy bazowej (Shape), nie do konkretnych implementacji.
-    //         Dzięki temu SvgScene nie musi wiedzieć nic o Polygon ani Ellipse -
-    //         wystarczy że wie że każdy Shape ma toSvg().
-    private Shape[] shapes = new Shape[3];
     private int index = 0;
+    private Shape[] shapes = new Shape[30];
 
-    public void addPolygon(Polygon polygon) {
-        addShape(polygon);
-    }
-
-    // Przyjmuje Shape - czyli może dostać Polygon, Ellipse, lub dowolny przyszły kształt
-    // bez zmiany tej metody. To jest siła polimorfizmu.
     public void addShape(Shape shape) {
-        shapes[index % shapes.length] = shape;
-        index++;
+        shapes[(index++) % shapes.length] = shape;
     }
 
-    public String toSvg() {
-        StringBuilder sb = new StringBuilder();
-        for (Shape s : shapes) {
-            if (s != null) {
-                // Tutaj działa polimorfizm:
-                // s.toSvg() wywoła Polygon.toSvg() jeśli s jest Polygon,
-                // albo Ellipse.toSvg() jeśli s jest Ellipse.
-                // My nie musimy sprawdzać typu - Java robi to automatycznie.
-                sb.append(s.toSvg()).append("\n");
-            }
+    private BoundingBox sceneBox() {
+        double maxX = 0, maxY = 0;
+        for(Shape shape: shapes) {
+            if(shape == null)
+                continue;
+            BoundingBox shapeBB = shape.boundingBox();
+            maxX = Math.max(maxX, shapeBB.x() + shapeBB.width());
+            maxY = Math.max(maxY, shapeBB.y() + shapeBB.height());
         }
-        return sb.toString();
+        return new BoundingBox(0, 0, maxX, maxY);
+    }
+
+    public String toSvg()
+    {
+        BoundingBox boundingBox = this.sceneBox();
+        String result = String.format(Locale.ENGLISH,
+                "<svg width=\"%f\" height=\"%f\" xmlns=\"http://www.w3.org/2000/svg\">",
+                boundingBox.width(), boundingBox.height());
+        for(var shape : shapes) {
+            if(shape ==  null)
+                continue;
+            result += "\n\t" + shape.toSvg();
+        }
+        result += "\n</svg>";
+        return result;
     }
 
     public void save(String path) throws IOException {
-        double maxX = 0, maxY = 0;
-        for (Shape s : shapes) {
-            // "instanceof" sprawdza czy obiekt jest danego typu.
-            // Używamy go tylko tam gdzie MUSIMY znać konkretny typ (tu: by wywołać boundingBox()).
-            // Zasada: unikaj instanceof gdzie możliwe - to sygnał że polimorfizm mógłby pomóc.
-            //         Tutaj Ellipse nie ma boundingBox(), więc musimy sprawdzić typ.
-            if (s instanceof Polygon p) {
-                BoundingBox bb = p.boundingBox();
-                if (bb.x() + bb.width()  > maxX) maxX = bb.x() + bb.width();
-                if (bb.y() + bb.height() > maxY) maxY = bb.y() + bb.height();
-            }
-        }
-
-        String svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + maxX + "\" height=\"" + maxY + "\">\n"
-                + toSvg()
-                + "</svg>";
-
-        try (FileWriter fw = new FileWriter(path)) {
-            fw.write(svg);
-        }
+        FileWriter writer = new FileWriter(path);
+        writer.write(toSvg());
+        writer.close();
     }
 }
